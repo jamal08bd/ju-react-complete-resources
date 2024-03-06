c4f6480e4d319fc5ff2ea7572924cfde60a60add
jamal08bd <jamal08.bd@gmail.com>
2024-03-06 14:17:33 +0200

    add: props  code snapshot

---
+++ code/ju-react-props/README.md
@@ -0,0 +1,8 @@
+## React: Props ##
+
+This code snapshot is an example of how we can use `props` in React, and how we receive them in the Component & print.
+
+To run this example:
+
+- `npm install` or `yarn`
+- `npm run dev` or `yarn dev`
---
+++ code/ju-react-props/src/App.jsx
@@ -0,0 +1,12 @@
+import Post from "./components/Post";
+
+function App() {
+  return (
+    <main>
+      <Post author="Jamal Uddin" body="I love how the React works!" />
+      <Post author="Rumana Akter" body="I am just learning React!" />
+    </main>
+  );
+}
+
+export default App;
