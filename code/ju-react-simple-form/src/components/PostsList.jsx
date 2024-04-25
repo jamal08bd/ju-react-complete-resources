7bd3912bb522f8a924c66dd7b606778965df070a
jamal08bd <jamal08.bd@gmail.com>
2024-04-25 22:09:50 +0300

    add: simple form

---
+++ code/ju-react-simple-form/src/components/PostsList.jsx
@@ -0,0 +1,39 @@
+import { useState } from "react";
+
+import Post from "./Post";
+import NewPost from "./NewPost";
+import Modal from "./Modal";
+import classes from "./PostsList.module.css";
+
+function PostsList({ isPosting, onStopPosting }) {
+  const [enteredBody, setEnteredBody] = useState("");
+  const [enteredAuthor, setEnteredAuthor] = useState("");
+
+  function bodyChangeHandler(event) {
+    setEnteredBody(event.target.value);
+  }
+
+  function authorChangeHandler(event) {
+    setEnteredAuthor(event.target.value);
+  }
+
+  return (
+    <>
+      {isPosting && (
+        <Modal onClose={onStopPosting}>
+          <NewPost
+            onBodyChange={bodyChangeHandler}
+            onAuthorChange={authorChangeHandler}
+            onCancel={onStopPosting}
+          />
+        </Modal>
+      )}
+      <ul className={classes.posts}>
+        <Post author={enteredAuthor} body={enteredBody} />
+        <Post author="Rumana Akter" body="I am just learning React!" />
+      </ul>
+    </>
+  );
+}
+
+export default PostsList;
