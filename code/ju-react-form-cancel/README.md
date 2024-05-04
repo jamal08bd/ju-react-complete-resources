fba89b71c368a8067f2bc1c584892892c981ae82
jamal08bd <jamal08.bd@gmail.com>
2024-05-04 12:12:11 +0300

    add: form cancel option

---
+++ code/ju-react-form-cancel/README.md
@@ -0,0 +1,8 @@
+## React: simple form ##
+
+This code snapshot is an example of how we can cancel a simple Form
+
+To run this example:
+
+- `npm install` or `yarn`
+- `npm run dev` or `yarn dev`
---
+++ code/ju-react-form-cancel/src/components/PostsList.jsx
@@ -0,0 +1,21 @@
+import Post from "./Post";
+import NewPost from "./NewPost";
+import Modal from "./Modal";
+import classes from "./PostsList.module.css";
+
+function PostsList({ isPosting, onStopPosting }) {
+  return (
+    <>
+      {isPosting && (
+        <Modal onClose={onStopPosting}>
+          <NewPost onCancel={onStopPosting} />
+        </Modal>
+      )}
+      <ul className={classes.posts}>
+        <Post author="Rumana Akter" body="I am just learning React!" />
+      </ul>
+    </>
+  );
+}
+
+export default PostsList;
