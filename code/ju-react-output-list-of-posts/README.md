73d0a6f2e5f91cea77d007d199ba60aab08c345e
jamal08bd <jamal08.bd@gmail.com>
2024-05-11 13:55:25 +0300

    add: insert and output list of posts

---
+++ code/ju-react-output-list-of-posts/README.md
@@ -0,0 +1,8 @@
+## React: Submit and output list of Posts ##
+
+This code snapshot is an example of how we can insert and shows list of posts
+
+To run this example:
+
+- `npm install` or `yarn`
+- `npm run dev` or `yarn dev`
---
+++ code/ju-react-output-list-of-posts/src/components/PostsList.jsx
@@ -0,0 +1,27 @@
+import Post from "./Post";
+import NewPost from "./NewPost";
+import Modal from "./Modal";
+import classes from "./PostsList.module.css";
+
+function PostsList({ isPosting, onStopPosting }) {
+  const [posts, setPosts] = useState([]);
+
+  function addPostHandler(postData) {
+    setPosts((existingPosts) => [postData, ...existingPosts]);
+  }
+
+  return (
+    <>
+      {isPosting && (
+        <Modal onClose={onStopPosting}>
+          <NewPost onCancel={onStopPosting} onAddPost={addPostHandler} />
+        </Modal>
+      )}
+      <ul className={classes.posts}>
+        <Post author="Rumana Akter" body="I am just learning React!" />
+      </ul>
+    </>
+  );
+}
+
+export default PostsList;
