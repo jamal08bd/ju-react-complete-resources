45bae2197fc6907862c5b9be5947310d1b0d1abf
jamal08bd <jamal08.bd@gmail.com>
2024-05-26 19:48:54 +0300

    update:  route

--- code/ju-react-adding-route/src/main.jsx
+++ code/ju-react-adding-route/src/main.jsx
@@ -3,10 +3,16 @@ import ReactDOM from 'react-dom/client'
 import { RouterProvider, createBrowserRouter } from 'react-router-dom';
 
 import App from './App'
+import NewPost from './components/NewPost';
 import './index.css'
+
+const router = createBrowserRouter([
+  { path: '/', element: <App /> },
+  { path: '/create-post', element: <NewPost /> }
+]);
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <RouterProvider />
+    <RouterProvider router={router} />
   </React.StrictMode>
 )
