158a480ce254f88ec856723b76d0e94925f8d76c
jamal08bd <jamal08.bd@gmail.com>
2024-05-26 11:54:23 +0300

    fix:  added missing code

--- code/ju-react-data-fetching-using-useeffect/src/components/PostsList.jsx
+++ code/ju-react-data-fetching-using-useeffect/src/components/PostsList.jsx
@@ -1,19 +1,29 @@
-import { useState } from "react";
+import { useState, useEffect } from 'react';
 
-import Post from "./Post";
-import NewPost from "./NewPost";
-import Modal from "./Modal";
-import classes from "./PostsList.module.css";
+import Post from './Post';
+import NewPost from './NewPost';
+import Modal from './Modal';
+import classes from './PostsList.module.css';
 
 function PostsList({ isPosting, onStopPosting }) {
   const [posts, setPosts] = useState([]);
 
+  useEffect(() => {
+    async function fetchPosts() {
+      const response = await fetch('http://localhost:8080/posts')
+      const resData = await response.json();
+      setPosts(resData.posts);
+    }
+
+    fetchPosts();
+  }, []);
+
   function addPostHandler(postData) {
-    fetch("http://localhost:8080/posts", {
-      method: "POST",
+    fetch('http://localhost:8080/posts', {
+      method: 'POST',
       body: JSON.stringify(postData),
       headers: {
-        "Content-Type": "application/json",
+        'Content-Type': 'application/json',
       },
     });
     setPosts((existingPosts) => [postData, ...existingPosts]);
@@ -34,7 +44,7 @@ function PostsList({ isPosting, onStopPosting }) {
         </ul>
       )}
       {posts.length === 0 && (
-        <div style={{ textAlign: "center", color: "white" }}>
+        <div style={{ textAlign: 'center', color: 'white' }}>
           <h2>There are no posts yet.</h2>
           <p>Start adding some!</p>
         </div>
